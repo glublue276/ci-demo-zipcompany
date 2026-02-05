e474893ac05a1d44ddb0fe8e08ab599f6f87b59c
Tanu Mukherjee <mukherjee.tanushree@gmail.com>
2026-02-05 10:21:45 -0800

    Adding Pre-commit and Pre-Push logic to fix linter errors

--- test/test_math_utils.py
+++ test/test_math_utils.py
@@ -1,24 +1,20 @@
 from src.math_utils import add, sub, mul, div
+
+
 def test_add():
-
-    
     assert add(1, 2) == 3
+
+
 def test_subtract():
-
-
     assert sub(1, 2) == -1
 
+
 def test_mul():
-
-
-    assert mul(2, 2 )== 4
+    assert mul(2, 2) == 4
     assert mul(-2, -2) == 4
     assert mul(2, -2) == -4
     assert mul(2, 0) == 0
+
+
 def test_div():
-
-
     assert div(2, 2) == 1
-
-
-
